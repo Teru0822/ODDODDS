f5db2dba30840ccecd12201976ef712423486375
IwamotoHinata <c612101277@tokushima-u.ac.jp>
2026-05-22 15:01:11 +0900

    feat:悪魔の取り立てにおける会話システム及びJsonファイルを用いたシナリオロード機能を作成

---
+++ Assets/Scripts/Devil/DevilConversationData.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+public enum DevilExpression
+{
+    Normal = 0,//通常
+    Mock = 1,//あざ笑う
+    Fun = 2,//楽しそう
+    Happy = 3,//嬉しい
+    Sad = 4,//悲しい
+    Angry = 5,//怒っている
+}
+
+[Serializable]
+public class DevilConversationData
+{
+    public string key;//会話のキー
+    public string nextKey;//次の会話のキー
+    public string[] lines;// デビルとの会話で登場するテキスト
+    public DevilExpression[] devilExpressions;//デビルの表情
+    public string bgmKey;//BGMのキー
+}
+
+
+[Serializable]
+public class DevilConversationContainer
+{
+    public List<DevilConversationData> conversations;
+}
+
